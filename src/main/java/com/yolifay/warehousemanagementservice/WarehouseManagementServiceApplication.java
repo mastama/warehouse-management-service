8a24da6083b76b620071ac3ac25611886ee1100e
Singgih Pratama <sp.mastama@gmail.com>
2025-10-23 06:09:25 +0700

    chore: init project spring boot warehouse management service

---
+++ src/main/java/com/yolifay/warehousemanagementservice/WarehouseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yolifay.warehousemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yolifay/warehousemanagementservice/WarehouseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolifay.warehousemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
